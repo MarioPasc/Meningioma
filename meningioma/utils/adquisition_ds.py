f229a8ee87a584616e8daf67f17e7ede5f4c04e2
MarioPasc <mario.pg02@gmail.com>
2024-08-17 17:07:07 +0200

    Image Adquisition based dataset transformed finished

--- meningioma/utils/adquisition_ds.py
+++ meningioma/utils/adquisition_ds.py
@@ -2,7 +2,7 @@ import os
 from natsort import natsorted
 import shutil
 from tqdm import tqdm
-import re
+import re # substring manipulation
 
 class DatasetTransformerAdquisition:
 
@@ -24,7 +24,9 @@ class DatasetTransformerAdquisition:
         # Patient folder within the raw data (exclude .xslx and other .log files)
         self.patient_folders = [folder for folder in os.listdir(self.source_dir) 
                                 if os.path.isdir(os.path.join(self.source_dir, folder))]
-        
+        # Sort them
+        self.patient_folders = natsorted(self.patient_folders)
+
         # Image technologies applied
         self.adquisition_types = ['RM', 'TC']
 
@@ -60,15 +62,12 @@ class DatasetTransformerAdquisition:
 
         Args:
             pulse (str): T1, T1SIN, SUSC or T2
-            verbose (bool): Show information messages in terminal about the state of the process
+            verbose (bool) = False: If set to true, display detailed process information in the terminal.
         """
 
         pulse_root_path = os.path.join('RM', pulse)
 
-        # Sort patient folders using natsort
-        sorted_patient_folders = natsorted(self.patient_folders)
-
-        for patient_folder in tqdm(iterable=sorted_patient_folders, desc=f'Moving {pulse} pulse ...', total=len(sorted_patient_folders)):
+        for patient_folder in tqdm(iterable=self.patient_folders, desc=f'Moving {pulse} pulse ...', total=len(self.patient_folders)):
             patient_path = os.path.join(self.source_dir, patient_folder)
             pulse_path = os.path.join(patient_path, pulse_root_path)
 
@@ -111,11 +110,84 @@ class DatasetTransformerAdquisition:
                 if verbose: print(f'Patient {patient_folder} does not have {pulse} pulse (missing folder).\n============')
 
 
-    def transform(self) -> None:
+    def _move_TC(self, verbose: bool = False) -> None:
+        """
+        Move the TC images from the raw dataset to the newly transformed dataset. 
+
+        Args:
+            verbose (bool) = False: If set to true, display detailed process information in the terminal.
+        """
+        for patient in tqdm(iterable=self.patient_folders, desc='Moving TC images ... ', total=len(self.patient_folders)):
+            patient_path = os.path.join(self.source_dir, patient)
+            tc_root = os.path.join(patient_path, 'TC')
+
+            if not os.path.exists(tc_root): # Some patients may have TC missing
+                if verbose: print(f'Patient {patient} does not have TC study (missing folder).')                    
+
+            else:
+                files = os.listdir(tc_root)
+                seg = False
+
+                if len(files) == 0:
+                    if verbose: print(f'Patient {patient} does not have TC study (missing files).')                    
+
+                else:
+
+                    if 'Segmentation.nrrd' not in files:  
+                        if verbose: print(f'Patient {patient} without Segmentation.nrrd')
+
+                    else:
+                        segmentation = files.pop(files.index('Segmentation.nrrd'))
+                        seg = True
+                    
+                    image = [file for file in files if (not file.endswith(('.png', '.seg.nrrd', '.mrml'))) 
+                             and (os.path.isfile(os.path.join(tc_root, file)) and file.endswith('.nrrd'))]
+                    if image:
+                        image = image[0]
+                    else:
+                        # This patient does not have any nrrd files (error while building the dataset) e.g. Patient 70
+                        continue
+                    # Some TC folders have other folders inside 
+                    if verbose: print(f'Patient: {patient}\n Segmentation: {segmentation}\n File: {image}\n============')
+
+                    # SECTION: Copy the image and segmentation files into the new patient folder for the specific pulse
+
+                    target_TC_folder = os.path.join(self.root, 'TC', f'P{patient}') 
+                    os.makedirs(target_TC_folder, exist_ok=True)
+
+                    # Copy and change the name of the image and segmentation files into the new transformed dataset structure
+                    new_image_name = f'TC_P{patient}.nrrd'
+                    image_source = os.path.join(tc_root, image)
+                    image_destination = os.path.join(target_TC_folder, new_image_name)
+                    shutil.copy2(image_source, image_destination)
+
+                    if seg: # If the image has a segmentation file ...
+                        new_segmentation_name = f'TC_P{patient}_seg.nrrd'
+                        segmentation_source = os.path.join(tc_root, segmentation)
+                        segmentation_destination = os.path.join(target_TC_folder, new_segmentation_name)
+                        shutil.copy2(segmentation_source, segmentation_destination)
+
+    def transform(self, verbose: bool = False) -> None:
+        """
+        Generate a new, clear dataset based on the image adquisition format from the raw data. 
+        
+        Calls:
+            self._generate_new_structure(): Generate folder hierarchy
+            self._move_RM_Pulse(pulse=pulse): Move the RM pulses 
+            self._move_TC(): Move TC images
+
+        Args:
+            verbose (bool) = False: If set to true, display detailed process information in the terminal.
+        """
+
+        # Generate the folder hierarchy
         self._generate_new_structure()
 
+        # Move and rename RM images
         for pulse in self.rm_pulses:
-            self._move_RM_Pulse(pulse=pulse)
+            self._move_RM_Pulse(pulse=pulse, verbose=verbose)
+
+        self._move_TC(verbose=verbose)
 
 def main() -> int:
     source = '/home/mariopasc/Python/Datasets/Meningiomas/Meningioma_RM_nrrd'
